71dd6bb4e5c4bc57c01da4d7155ecd17435f5365
Stephen Yorke <syorke@gmail.com>
2016-08-08 11:50:55 -0300

    Add variables to get ready for other dists

--- instjenkins.bash
+++ instjenkins.bash
@@ -5,19 +5,51 @@
 # If issues are found, please submit update and push changes if you
 #   are comfortable with using Git and BASH scripting
 
-# Get and install the Jenkins signing key
-wget -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
+# Constant variables
+CURDIR="$(pwd)"
+RMRF="rm -Rf"
+RMF="rm -f"
+LOGFILE="/var/log/log_Jenkins_`date '+%F_%H-%M-%S'`.log"
+numProcs="$(( $(cat /proc/cpuinfo | grep processor | wc -l) + 1 ))"
+ETC="/etc"
+ETCINIT="$ETC/init"
+ETCINITd="$ETC/init.d"
+USRLOCALBIN="/usr/local/bin"
+thisArch="$(uname -m)"
+myRunLVL=$(runlevel | awk '{ print $2 }')
 
-# Check for Java and if exist, remove and install OpenJDK
-JAVAINST="$(java -version)"
-
-if [ "$JAVAINST" = "" ]; then
-   apt-get install openjdk-7-jre -y
+if [ -e /usr/bin/lsb_release ]; then
+   thisDist="$(lsb_release -i | awk '{ print $3 }')"
+   thisRel="$(lsb_release -r | awk '{ print $2 }' | cut -d '.' -f1)"
+else
+   thisDist="$(grep "^NAME=" $ETC/os-release | cut -d "=" -f2 | tr -d "\"" | awk '{ print $1 }')"
+   thisRel="$(grep "^VERSION=" $ETC/os-release | cut -d "=" -f2 | tr -d "\"" | cut -d " " -f1 | cut -d "." -f1)"
 fi
 
-if [ ! -e /etc/apt/sources.list.d/jenkins.list ]; then
-   echo "deb http://pkg.jenkins-ci.org/debian-stable binary/" > /etc/apt/sources.list.d/jenkins.list
-   apt-get update && apt-get install jenkins -y
-fi
+
+case "$thisDist" in
+   "Debian"|"Ubuntu")
+      # Get and install the Jenkins signing key
+      wget -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | apt-key add -
+
+      # Check for Java and if exist, remove and install OpenJDK
+      JAVAINST="$(java -version)"
+
+      if [ "$JAVAINST" = "" ]; then
+         apt-get install openjdk-7-jre -y
+      fi
+
+      # Setup Jenkins repo and install
+      if [ ! -e /etc/apt/sources.list.d/jenkins.list ]; then
+         echo "deb http://pkg.jenkins-ci.org/debian-stable binary/" > /etc/apt/sources.list.d/jenkins.list
+         apt-get update && apt-get install jenkins -y
+      fi
+   ;;
+
+   *)
+      echo "$thisDist is currently not supported"
+   ;;
+
+esac
 
 echo "...Complete..."
